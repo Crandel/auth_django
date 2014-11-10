7baecbbbdc8e09a178ccddf2ab34d4eff03512c2
stepanov <stepanov@steelkiwi.com>
2014-11-10 17:44:21 +0200

    Add contact us

--- apps/contactus/models.py
+++ apps/contactus/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.sites.models import Site
 
+
 class ContactUs(models.Model):
     """
     Model for managing contact us page inner section of form.
@@ -9,10 +10,12 @@ class ContactUs(models.Model):
     """
     site = models.ForeignKey(Site)
     name = models.CharField(_('Name'), max_length=100,)
+    company_name = models.CharField(_('Company Name'), max_length=100, null=True)
     email = models.EmailField(_('Email Address'), max_length=100,)
     telephone = models.CharField(_('Telephone'), max_length=100,)
-    enquiry = models.TextField(_('Enquiry'))
-    date = models.DateTimeField(_('Contacted Time'),)
+    subject = models.CharField(_('Subject'), null=True, max_length=255)
+    details = models.TextField(_('Details'), null=True)
+    date = models.DateTimeField(_('Contacted Time'), auto_now=True)
 
     def __unicode__(self):
         return self.site.name
@@ -20,7 +23,7 @@ class ContactUs(models.Model):
     class Meta:
         verbose_name = _('Contact Us')
         verbose_name_plural = _('Contact Us')
-    
+
 
 class GoogleContact(models.Model):
     """
