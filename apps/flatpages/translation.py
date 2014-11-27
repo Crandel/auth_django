7ad4a9b7791ec016cbc7568fb4fcb140d3c0d289
Vitaly Drevenchuk <drevenchuk@steelkiwi.com>
2014-11-27 17:29:55 +0200

    start project Auth

--- apps/flatpages/translation.py
+++
@@ -1,8 +0,0 @@
-from modeltranslation.translator import translator, TranslationOptions
-from django.contrib.flatpages.models import FlatPage
-
-
-class FlatpageTranslationOptions(TranslationOptions):
-    fields = ('title', 'content',)
-
-translator.register(FlatPage, FlatpageTranslationOptions)
--- apps/htmlsitemap/views.py
+++
@@ -1,5 +0,0 @@
-
-from django.views.generic.base import TemplateView
-
-class Sitemapiew(TemplateView):
-    template_name = "htmlsitemap.html"
